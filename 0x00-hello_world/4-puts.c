206ffaab2d164405870afe99168c9dedfbda4bde
nehemiah ogah <nehmarrhogah@gmail.com>
2022-09-08 18:14:18 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+
+/**
+ *main - printing
+ *
+ *Return: Always 0 (successful)
+ */
+int main(void)
+{
+         puts("\"Programming is like building multilingual puzzle");
+         return (0);
+}
