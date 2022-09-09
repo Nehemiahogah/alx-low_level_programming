c81d8e43585c36c49dd8c4c548609976de401500
nehemiah ogah <nehmarrhogah@gmail.com>
2022-09-09 02:51:05 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
-*main - Prints the text
-*Return - Always 1 (successful), non 1 valueis an erroe code   
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
+
+
+
