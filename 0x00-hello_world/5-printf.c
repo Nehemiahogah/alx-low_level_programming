29ab20e800060b92e13986641ae56a02a27977cf
nehemiah ogah <nehmarrhogah@gmail.com>
2022-09-08 19:04:01 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,3 @@
 #include <stdio.h>                          /**
-*main - Entry point                         *Return: Always 0 (successful)              */                                          int main(void)                              {
+*main - Prints a text                       *Return: Always 0 (success)                 */                                          int main(void)                              {
 printf("with proper grammar, but the outcome is a piece of art,\n");                    return (0);                                 }
