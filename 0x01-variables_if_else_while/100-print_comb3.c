602998d3cd5425e1390c528ecef034b20e5bcb3d
nehemiah ogah <nehmarrhogah@gmail.com>
2022-09-09 19:08:32 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,12 +8,16 @@
 int main(void)
 {
 int d;
-for (d = '0'; d < '100'; d++)
+for (d = 0; d < 100; d++)
+{
 putchar((d / 10) + '0');
 putchar((d % 10) + '0');
 if (d != 99)
+{
 putchar(',');
 putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
