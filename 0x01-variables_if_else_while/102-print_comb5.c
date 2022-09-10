1d71ecbbd9313ff415e01b8f20fea64e712671c8
nehemiah ogah <nehmarrhogah@gmail.com>
2022-09-10 06:29:00 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,15 @@ int main(void)
 int n1 = 0, n2;
 while (n1 <= 98)
 {
-n2 = n1;
+n2 = n1 + 1;
 while (n2 <= 99)
 {
-if (n2 != n1)
-{
-putchar((n1 / 10) + 48);
-putchar((n1 % 10) + 48);
+putchar((n1 / 10) + '0');
+putchar((n1 % 10) + '0');
 putchar(' ');
-putchar((n2 / 10) + 48);
-putchar((n2 % 10) + 48);
-if (n1 != 98 || n2 != 98)
+putchar((n2 / 10) + '0');
+putchar((n2 % 10) + '0');
+if (n1 == 98 || n2 == 98)
 {
 putchar(',');
 putchar(' ');
