afe221c9a0eefaa10934d21c952caaf02124d1dd
nehemiah ogah <nehmarrhogah@gmail.com>
2022-09-09 20:38:47 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,11 @@ while (n1 <= 9)
 {
 n2 = 0;
 while (n2 <= 9)
+{
 if (n1 != n2 && n1 < n2)
 {
-putchar(n1 + 48)
-putchat(n2 + 48)
+putchar(n1 + 48);
+putchat(n2 + 48);
 if(n1 + n2 != 17)
 {
 putchar(',');
