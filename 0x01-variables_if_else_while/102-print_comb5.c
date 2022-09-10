73cbdf49eb1886274287d80c5a8af18a7287bb7f
nehemiah ogah <nehmarrhogah@gmail.com>
2022-09-10 06:08:12 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,25 +3,33 @@
 #include <time.h>
 /**
 * main - Entry point
-* Return: Print
+* Return: Always 0 (success)
 */
 int main(void)
 {
 int n1 = 0, n2;
-while (n1 <= 99)
+while (n1 <= 98)
+{
 n2 = n1;
 while (n2 <= 99)
+{
 if (n2 != n1)
+{
 putchar((n1 / 10) + 48);
 putchar((n1 % 10) + 48);
 putchar(' ');
 putchar((n2 / 10) + 48);
 putchar((n2 % 10) + 48);
 if (n1 != 98 || n2 != 98)
+{
 putchar(',');
 putchar(' ');
+}
+}
 ++n2;
+}
 ++n1;
+}
 putchar('\n');
 return (0);
 }
